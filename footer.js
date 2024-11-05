71d2f6b36a5fa46c2ffeb2ae8d4476f529e9fa17
Chris McKee <chrismckee07@gmail.com>
2024-11-05 23:58:04 +1100

    Initial push of Katrina McKee website version 3

---
+++ footer.js
@@ -0,0 +1,13 @@
+var footerItems = `<footer>
+<div class="footer">
+  <ul>
+    <li class="socialLink"><a href="https://twitter.com/katrinamckee03"><img src="./images/x_twitter_icon.png" alt="X Twitter Link"></a></li>
+    <li class="socialLink"><a href="https://www.instagram.com/katrinakmckee/"><img src="./images/instagram_icon.svg" alt="Instagram Link"></a></li>
+    <li class="socialLink"><a href="https://www.tumblr.com/katrinamckee03"><img src="./images/tumblr_icon.png" alt="Tumblr Link"></a></li>
+    <li class="socialLink"><a href="https://katrinamckee03.substack.com/"><img src="./images/substack.png" alt="Substack Link"></a></li>
+  </ul>
+  <p class="copyright-text">Copyright © 2024</p>  
+</div>
+</footer>
+`
+document.body.insertAdjacentHTML("afterend", footerItems);
---
+++ header.js
@@ -0,0 +1,11 @@
+var headerHTML = `<div>
+<header class="header">
+  <a class="navlink brand" href="index.html">Katrina<br>&nbsp &nbsp &nbsp McKee</a>
+  <a class="navlink" href="books.html">Books</a>
+  <a class="navlink" href="about.html">About</a>
+  <a class="navlink" href="contact.html">Contact</a>
+  <a class="navlink" href="news.html">News</a>
+</header>
+</div>
+`
+document.body.insertAdjacentHTML("beforebegin", headerHTML);
