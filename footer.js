becc64f7bcb347defa6cf536c06e59d8924c5c57
Chris McKee <chrismckee07@gmail.com>
2024-11-24 22:36:21 +1100

    Make header look nicer plus some formatting changes on footer

--- footer.js
+++ footer.js
@@ -1,16 +1,18 @@
-var footerItems = `<footer>
-<div class="footer">
-  Follow me on: 
-  <ul>
-    <li class="socialLink"><a href="https://twitter.com/katrinamckee03"><img src="./images/x_twitter_icon.png" alt="X Twitter Link"></a></li>
-    <li class="socialLink"><a href="https://www.instagram.com/katrinakmckee/"><img src="./images/instagram_icon.svg" alt="Instagram Link"></a></li>
-    <li class="socialLink"><a href="https://www.tumblr.com/katrinamckee03"><img src="./images/tumblr_icon.png" alt="Tumblr Link"></a></li>
-    <li class="socialLink"><a href="https://katrinamckee03.substack.com/"><img src="./images/substack.png" alt="Substack Link"></a></li>
-    <li class="socialLink"><a href="https://www.threads.net/@katrinakmckee"><img src="./images/Threads-Logo.png" alt="Substack Link"></a></li>
-    <li class="socialLink"><a href="katrinamckee03.bsky.social"><img src="./images/Bluesky_Logo.png" alt="Bluesky Link"></a></li>
-  </ul>
-  <p class="copyright-text">Copyright © 2024</p>  
-</div>
+var footerItems = `
+<footer class="footer">
+  <div class="social-list">
+    <ul>
+      <li class="socialLink"><a href="https://twitter.com/katrinamckee03"><img src="./images/x_twitter_icon.png" alt="X Twitter Link"></a></li>
+      <li class="socialLink"><a href="https://www.instagram.com/katrinakmckee/"><img src="./images/instagram_icon.svg" alt="Instagram Link"></a></li>
+      <li class="socialLink"><a href="https://www.tumblr.com/katrinamckee03"><img src="./images/tumblr_icon.png" alt="Tumblr Link"></a></li>
+      <li class="socialLink"><a href="https://katrinamckee03.substack.com/"><img src="./images/substack.png" alt="Substack Link"></a></li>
+      <li class="socialLink"><a href="https://www.threads.net/@katrinakmckee"><img src="./images/Threads-Logo.png" alt="Substack Link"></a></li>
+      <li class="socialLink"><a href="katrinamckee03.bsky.social"><img src="./images/Bluesky_Logo.png" alt="Bluesky Link"></a></li>
+    </ul>
+  </div>
+  <div class="copyright-text">
+    <p>Copyright © 2024</p>  
+  </div>
 </footer>
 `
 document.body.insertAdjacentHTML("afterend", footerItems);
