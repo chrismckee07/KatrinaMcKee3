f958a1e9facdde08e6972c8f221a6fb5141dc122
Chris McKee <chrismckee07@gmail.com>
2025-11-30 22:47:21 +1100

    Updated alt text for footer imgs

--- footer.js
+++ footer.js
@@ -8,8 +8,8 @@ var footerItems = `
       <li class="socialLink"><a href="https://katrinamckee03.substack.com/"><img src="./images/substack.png" alt="Substack Link"></a></li>
       <li class="socialLink"><a href="https://www.threads.net/@katrinakmckee"><img src="./images/Threads-Logo.png" alt="Substack Link"></a></li>
       <li class="socialLink"><a href="https://www.katrinamckee03.bsky.social"><img src="./images/Bluesky_Logo.png" alt="Bluesky Link"></a></li>
-      <li class="socialLink"><a href="https://www.goodreads.com/author/show/50873996.Katrina_McKee"><img src="./images/goodreads-icon-1243251135.png" alt="Bluesky Link"></a></li>
-      <li class="socialLink"><a href="https://www.facebook.com/profile.php?id=61579110203888"><img src="./images/facebook-logo-facebook-icon-transparent-free-png-1237058317.png" alt="Bluesky Link"></a></li>
+      <li class="socialLink"><a href="https://www.goodreads.com/author/show/50873996.Katrina_McKee"><img src="./images/goodreads-icon-1243251135.png" alt="Goodreads Link"></a></li>
+      <li class="socialLink"><a href="https://www.facebook.com/profile.php?id=61579110203888"><img src="./images/facebook-logo-facebook-icon-transparent-free-png-1237058317.png" alt="Facebook Link"></a></li>
       <li class="socialLink"><a href=mailto:katrinamckee03@gmail.com”><img src="./images/mail_icon.png" alt="email link"></a></li>
     </ul>
   </div>
