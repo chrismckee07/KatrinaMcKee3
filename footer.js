837756ff9886443977ea6de449a6712b8783fe78
Chris McKee <chrismckee07@gmail.com>
2026-06-16 23:48:56 +1000

    Make footer links open in a new tab instead

--- footer.js
+++ footer.js
@@ -2,15 +2,15 @@ var footerItems = `
 <footer class="footer">
   <div class="social-list">
     <ul>
-      <li class="socialLink"><a href="https://twitter.com/katrinamckee03"><img src="./images/x_twitter_icon.png" alt="X Twitter Link"></a></li>
-      <li class="socialLink"><a href="https://www.instagram.com/katrinakmckee/"><img src="./images/instagram_icon.svg" alt="Instagram Link"></a></li>
-      <li class="socialLink"><a href="https://www.tumblr.com/katrinamckee03"><img src="./images/tumblr_icon.png" alt="Tumblr Link"></a></li>
-      <li class="socialLink"><a href="https://katrinamckee03.substack.com/"><img src="./images/substack.png" alt="Substack Link"></a></li>
-      <li class="socialLink"><a href="https://www.threads.net/@katrinakmckee"><img src="./images/Threads-Logo.png" alt="Substack Link"></a></li>
-      <li class="socialLink"><a href="https://www.katrinamckee03.bsky.social"><img src="./images/Bluesky_Logo.png" alt="Bluesky Link"></a></li>
-      <li class="socialLink"><a href="https://www.facebook.com/profile.php?id=61590870175415"><img src="./images/facebook-logo-facebook-icon-transparent-free-png-1237058317.png" alt="Facebook Link"></a></li>
-      <li class="socialLink"><a href="https://www.goodreads.com/author/show/50873996.Katrina_McKee"><img src="./images/goodreads-icon-1243251135.png" alt="Goodreads Link"></a></li>
-      <li class="socialLink"><a href=mailto:katrinamckee03@gmail.com”><img src="./images/mail_icon.png" alt="email link"></a></li>
+      <li class="socialLink"><a href="https://twitter.com/katrinamckee03" target="_blank"><img src="./images/x_twitter_icon.png" alt="X Twitter Link"></a></li>
+      <li class="socialLink"><a href="https://www.instagram.com/katrinakmckee/" target="_blank"><img src="./images/instagram_icon.svg" alt="Instagram Link"></a></li>
+      <li class="socialLink"><a href="https://www.tumblr.com/katrinamckee03" target="_blank"><img src="./images/tumblr_icon.png" alt="Tumblr Link"></a></li>
+      <li class="socialLink"><a href="https://katrinamckee03.substack.com/" target="_blank"><img src="./images/substack.png" alt="Substack Link"></a></li>
+      <li class="socialLink"><a href="https://www.threads.net/@katrinakmckee" target="_blank"><img src="./images/Threads-Logo.png" alt="Substack Link"></a></li>
+      <li class="socialLink"><a href="https://www.katrinamckee03.bsky.social" target="_blank"><img src="./images/Bluesky_Logo.png" alt="Bluesky Link"></a></li>
+      <li class="socialLink"><a href="https://www.facebook.com/profile.php?id=61590870175415" target="_blank"><img src="./images/facebook-logo-facebook-icon-transparent-free-png-1237058317.png" alt="Facebook Link"></a></li>
+      <li class="socialLink"><a href="https://www.goodreads.com/author/show/50873996.Katrina_McKee" target="_blank"><img src="./images/goodreads-icon-1243251135.png" alt="Goodreads Link"></a></li>
+      <li class="socialLink"><a href=mailto:katrinamckee03@gmail.com” target="_blank"><img src="./images/mail_icon.png" alt="email link"></a></li>
     </ul>
   </div>
   <div class="copyright-text">
