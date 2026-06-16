47df5c320075dbcf4341f76f620bd27dd9137f9a
Chris McKee <chrismckee07@gmail.com>
2026-06-16 23:58:24 +1000

    Fix broken blue sky link

--- footer.js
+++ footer.js
@@ -7,7 +7,7 @@ var footerItems = `
       <li class="socialLink"><a href="https://www.tumblr.com/katrinamckee03" target="_blank"><img src="./images/tumblr_icon.png" alt="Tumblr Link"></a></li>
       <li class="socialLink"><a href="https://katrinamckee03.substack.com/" target="_blank"><img src="./images/substack.png" alt="Substack Link"></a></li>
       <li class="socialLink"><a href="https://www.threads.net/@katrinakmckee" target="_blank"><img src="./images/Threads-Logo.png" alt="Substack Link"></a></li>
-      <li class="socialLink"><a href="https://www.katrinamckee03.bsky.social" target="_blank"><img src="./images/Bluesky_Logo.png" alt="Bluesky Link"></a></li>
+      <li class="socialLink"><a href="https://bsky.app/profile/katrinamckee03.bsky.social" target="_blank"><img src="./images/Bluesky_Logo.png" alt="Bluesky Link"></a></li>
       <li class="socialLink"><a href="https://www.facebook.com/profile.php?id=61590870175415" target="_blank"><img src="./images/facebook-logo-facebook-icon-transparent-free-png-1237058317.png" alt="Facebook Link"></a></li>
       <li class="socialLink"><a href="https://www.goodreads.com/author/show/50873996.Katrina_McKee" target="_blank"><img src="./images/goodreads-icon-1243251135.png" alt="Goodreads Link"></a></li>
       <li class="socialLink"><a href=mailto:katrinamckee03@gmail.com” target="_blank"><img src="./images/mail_icon.png" alt="email link"></a></li>
